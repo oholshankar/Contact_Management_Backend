9724b5f09615df082ebf75f475d86c845059f1c6
oholshankar <oholshankar@gmail.com>
2024-10-14 04:20:54 +0530

    Optimize extra api Call | Changes in return type of CRUD operations to avoid get call after every action

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -27,44 +27,44 @@ namespace Contact_Management.Controllers
         {
                 var contacts = ContactDbHelper.ReadFromJsonFile<Contact>();
                 var contactFirst = contacts.FirstOrDefault(p => p.Id == id);
-
                 return contactFirst;
  
         }
 
         [HttpPost]
-        public ActionResult<Contact> CreateContact([FromBody] Contact newContact)
+        public ActionResult<List<Contact>> CreateContact([FromBody] Contact newContact)
         {
             var contacts = ContactDbHelper.ReadFromJsonFile<Contact>();
             var MaxId= contacts.OrderByDescending(p => p.Id).FirstOrDefault();
             newContact.Id = MaxId!=null ? Convert.ToInt32(MaxId.Id) + 1 : 1;
             contacts.Add(newContact);
             ContactDbHelper.WriteToJsonFile(contacts);
-            return CreatedAtAction(nameof(Getcontact), new { id = newContact.Id }, newContact);
+            return ContactDbHelper.ReadFromJsonFile<Contact>();
         }
 
         [HttpPut("{id}")]
-        public ActionResult UpdateContact(int id, [FromBody] Contact updatedContact)
+        public ActionResult<List<Contact>> UpdateContact(int id, [FromBody] Contact updatedContact)
         {
             var contacts = ContactDbHelper.ReadFromJsonFile<Contact>();
             var contact = contacts.FirstOrDefault(p => p.Id == id);
 
             contact.FirstName = updatedContact.FirstName;
             contact.LastName = updatedContact.LastName;
+            contact.Email = updatedContact.Email;
             ContactDbHelper.WriteToJsonFile(contacts);
 
-            return NoContent();
+            return ContactDbHelper.ReadFromJsonFile<Contact>();
         }
 
         [HttpDelete("{id}")]
-        public ActionResult DeleteContact(int id)
+        public ActionResult<List<Contact>> DeleteContact(int id)
         {
             var contacts = ContactDbHelper.ReadFromJsonFile<Contact>();
             var contact = contacts.FirstOrDefault(p => p.Id == id);
             contacts.Remove(contact);
             ContactDbHelper.WriteToJsonFile(contacts);
 
-            return NoContent();
+            return ContactDbHelper.ReadFromJsonFile<Contact>();
         }
     }
 }
