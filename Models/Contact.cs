db57cf2d44cac0fc290e630e8f71bb4da33c4d8c
oholshankar <oholshankar@gmail.com>
2024-10-14 12:10:34 +0530

    As Per user story Validation added for fields FirstName,LastName,Email

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,12 +1,21 @@
-﻿namespace Contact_Management.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contact_Management.Models
 {
     public class Contact
     {
+        [Key]
         
         public int Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
 
+        
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
